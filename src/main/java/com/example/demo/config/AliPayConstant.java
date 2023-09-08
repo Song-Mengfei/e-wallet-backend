bb033e101128fe225e57471bbe007c5839cc602a
Song-Mengfei <e1075530@u.nus.edu>
2023-09-08 23:23:12 +0800

    alypay

---
+++ src/main/java/com/example/demo/config/AliPayConstant.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+public class AliPayConstant {
+
+    public static final String appId = "appId";
+
+    public static final String alipayPublicKey = "alipayPublicKey";
+
+    public static final String merchantCertPath = "merchantCertPath";
+
+    public static final String alipayCertPath = "alipayCertPath";
+
+    public static final String alipayRootCertPath = "alipayRootCertPath";
+
+    public static final String gatewayHost = "gatewayHost";
+
+    public static final String protocol = "protocol";
+
+    public static final String signType = "signType";
+
+    public static final String merchantPrivateKey = "merchantPrivateKey";
+
+    public static final String notifyUrl = "notifyUrl";
+
+    public static final String timeoutExpress = "timeoutExpress";
+
+    public static final String webReturnUrl = "webReturnUrl";
+}
